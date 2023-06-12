8f76c712d005af81357f43bf0785c52fe6942b5e
Evgenii Kinchinpnpm <kinchin@mail.ru>
2023-06-12 20:15:37 +0500

    скропт добавления катушки, альтер для картинок берется из описания

---
+++ add-new-roll.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+TARGET="src/rolls/$1"
+SOURCE="new-roll"
+ROOT=`pwd`
+mkdir "${TARGET}"
+mkdir "${TARGET}/photos"
+cp ${SOURCE}/index.md "${TARGET}"
+
+cd ${SOURCE}/photos/
+for image in *.jpg; do
+  convert $image -resize 480 $ROOT/$TARGET/photos/xs-$image
+  convert $image -resize 1920 $ROOT/$TARGET/photos/$image
+done
